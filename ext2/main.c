6b620745362d5b4da61ecce1174288754d6b530d
Alex Bahm <alexgbahm@gmail.com>
2014-10-31 16:26:09 -0700

    Renamed lab6 to ext2 and created files for upcomfing functions

---
+++ ext2/main.c
@@ -0,0 +1,68 @@
+#include "type.h"
+#include "utility.h"
+#include "proc.h"
+#include "fileops.h"
+
+char line[128], cmd[32], pathname[64];
+extern PROC *running;
+extern MINODE *root;
+char *diskimage = "fdimage";
+
+int main() {
+	fs_init(diskimage);   // initialize FS data structures
+
+	while(1){ 
+		// ask for a command LINE; extract cmd, pathname from line:
+
+
+		printf("Command: ");
+		fgets(line, 128, stdin);
+		line[strlen(line)-1] = 0;  // kill the \n char at end
+
+		getCMD(line);
+		getPath(line);
+
+		if (line[0]==0) {
+			continue;
+		}
+
+		printf("CMD: %s PATH: %s\n", cmd, pathname);
+		if (strEq(cmd, "ls")) {
+			ls(pathname);
+		} else if (strEq(cmd, "cd")) {
+			cd(pathname);
+		} else if (strEq(cmd, "pwd")) {
+			pwd(running->cwd);
+		} else if (strEq(cmd, "quit") || strEq(cmd, "q")) {
+			quit(); // write back any dirty in-memory INODE; exit(0);
+			exit(0);
+		} else if(strEq(cmd, "procs")) {
+			printAllProcs();
+		} else if(strEq(cmd, "minodes")) {
+			printAllMINodes();
+		} else if(strEq(cmd, "mounts")) {
+			printAllMounts();
+		}
+	}
+}
+
+int getCMD(char *line) {
+	char* copy = strdup(line); //ensure we don't mess up the original
+	strcpy(cmd, strtok(copy, " "));
+	free(copy);
+	return;
+}
+
+int getPath(char *line) {
+	char* copy = strdup(line); //ensure we don't mess up the original
+	strtok(copy, " "); //This will be the command, ignore 
+	char* temp = strtok(NULL, " ");
+
+	if(temp) {
+		strcpy(pathname, temp);
+	} else {
+		pathname[0] = 0; //Make the string empty if there is no path
+	}
+
+	free(copy); //free unneeded memory
+}
