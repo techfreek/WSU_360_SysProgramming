ea20f36f8ff14a229a08a000064a5d2363742a53
Alex Bahm <alexgbahm@gmail.com>
2014-11-04 00:52:50 -0800

    Decided to work a tiny bit on removing an entry

--- ext2/rm.c
+++ ext2/rm.c
@@ -6,6 +6,7 @@
 #include "utility.h"
 #include "fileops.h"
 #include "minode.h"
+#include "mk.c"
 
 extern PROC *running;
 extern MINODE *root;
@@ -51,5 +52,53 @@ int  myrmdir(char *path) {
 		printf("Specified directory (%s) is not a directory.\n", path);
 	}
 }
+
+
+int removechild(int ino, int devId) {
+	int i = 0;
+	INODE *ip = get_inode(devId, ino);
+	char *cp, *pp;
+	DIR *child;
+
+	for(; (i < SINGLEINDIRECT) && (ip->i_block[i] != 0); i++) {
+		get_block(devId, ip->i_block[i], buf);
+
+		if(cp = inodeExists(ino, buf, &pp)) { //this is intentional so we don't have to scan again
+			child = (SHUTUP)cp;
+			/*
+				if child size == 1024  -> delete block
+				if child size > ideal  -> increase length of previous entry cover us
+				if child size == ideal -> shift blocks over from the right and extend the length of the last entry
+
+			*/
+
+
+		}
+
+	}
+	printf("Unknown problem removing file\n");
+}
+
+char* inodeExists(int ino, char buf[], char **pp) {
+	int i = 0;
+	INODE pInode = parent->INODE;
+	char buf[BLKSIZE];
+	char *cp;
+	DIR *dp;
+
+	dp = (DIR *)buf;
+	cp = buf;
+
+	while(cp < (buf + 1024)) {
+		pp = cp; //in case the first one is the one we are looking for
+		if(dp->inode == ino) {
+			return cp;
+		}
+		cp += dp->rec_len;
+		dp = (SHUTUP)cp;
+	}
+	return NULL;
+}
+
 
 #endif
